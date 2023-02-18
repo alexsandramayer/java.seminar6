4e660d75e0e71954503f5192b149815ae929f157
Alexsandra Mayer <97391174+alexsandramayer@users.noreply.github.com>
2023-02-18 12:14:08 +0500

    Add files via upload

---
+++ catalog.java
@@ -0,0 +1,10 @@
+package seminar6;
+import java.util.*;
+
+public class catalog extends laptop {
+
+    public catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+}
+
